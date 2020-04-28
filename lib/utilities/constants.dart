f82005a6a67f761379e8c0baf0421d6fd1d514a7
mhabdulbaaki <mohammedhardi82@gmail.com>
2020-04-28 13:00:50 +0100

    initial commit

---
+++ lib/utilities/constants.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+const kTempTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 100.0,
+);
+
+const kMessageTextStyle = TextStyle(
+  fontFamily: 'Spartan MB',
+  fontSize: 60.0,
+);
+
+const kButtonTextStyle = TextStyle(
+  fontSize: 30.0,
+  fontFamily: 'Spartan MB',
+);
+
+const kConditionTextStyle = TextStyle(
+  fontSize: 100.0,
+);
+
+const kInputDecorationTextField = InputDecoration(
+  fillColor: Colors.white,
+  filled: true,
+  hintText: 'Enter City Name',
+  hintStyle: TextStyle(
+    color: Colors.grey,
+  ),
+  border: OutlineInputBorder(
+      borderRadius: BorderRadius.all(
+        Radius.circular(10),
+      ),
+      borderSide: BorderSide.none),
+);
